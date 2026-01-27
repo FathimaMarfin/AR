9c0d5485131fd97da841ac016e9c53bc2ec31eaa
Fathima Marfin <fathimamarfin@gmail.com>
2026-01-27 23:15:05 +0530

    Fixed router syntax error

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import {createBrowserRouter,} from "react-router";
+import {createBrowserRouter} from "react-router";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
